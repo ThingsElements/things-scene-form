efc7a4cd159c23f3ffe5ff3ed5c50797da88f523
Stellan Gil <lotstar87@gmail.com>
2017-10-30 11:02:31 +0900

    v0.1.12
    label format 수정.

--- src/form.js
+++ src/form.js
@@ -54,7 +54,7 @@ const NATURE = {
     }
   }, {
     type: 'checkbox',
-    label: 'withCredentials',
+    label: 'with-credentials',
     name: 'withCredentials',
     property: 'withCredentials'
   }, {
