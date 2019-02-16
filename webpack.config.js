42d38c862b31e4fdc84fa331c35a1953afac95f9
Hearty Oh <heartyoh@hatiolab.com>
2019-02-16 14:29:56 +0900

    module for ie11

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 module.exports = {
   mode: 'production',
   entry: {
-    'things-scene-form': ['./src/index.js']
+    'things-scene-form-ie': ['./src/index.js']
   },
   output: {
     path: path.resolve('./dist'),
@@ -29,19 +29,23 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                targets: {
-                  browsers: ['last 2 versions', 'safari >= 7', 'ie 11']
-                }
-              }
-            ]
-          ]
-        }
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: {
+                      ie: 11
+                    }
+                  }
+                ]
+              ]
+            }
+          }
+        ]
       },
       {
         test: /\.(gif|jpe?g|png)$/,
